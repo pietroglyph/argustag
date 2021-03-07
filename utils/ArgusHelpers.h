f25e718a4b1512866d23c5a0c5b498c7bf92b929
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2021-03-06 20:09:55 -0500

    Format utils folder and remove unsued helpers
    ArgusHelpers will be removed in the future.

--- utils/ArgusHelpers.h
+++ utils/ArgusHelpers.h
@@ -31,12 +31,9 @@
 
 #include <Argus/Argus.h>
 
-namespace ArgusSamples
-{
+namespace ArgusSamples {
 
-namespace ArgusHelpers
-{
-
+namespace ArgusHelpers {
 
 /**
  * Returns the CameraDevice of a given index from a CameraProvider.
@@ -44,7 +41,7 @@ namespace ArgusHelpers
  * @param[in] camera_provider The CameraProvider to get the device from.
  * @param[in] cameraDeviceIndex The index of the device to get.
  */
-Argus::CameraDevice* getCameraDevice(Argus::CameraProvider* camera_provider,
+Argus::CameraDevice *getCameraDevice(Argus::CameraProvider *camera_provider,
                                      uint32_t cameraDeviceIndex);
 
 /**
@@ -53,7 +50,7 @@ Argus::CameraDevice* getCameraDevice(Argus::CameraProvider* camera_provider,
  * @param[in] cameraDevice The CameraDevice to get the sensor mode from.
  * @param[in] sensorModendex The index of the sensor mode to get.
  */
-Argus::SensorMode* getSensorMode(Argus::CameraDevice* cameraDevice,
+Argus::SensorMode *getSensorMode(Argus::CameraDevice *cameraDevice,
                                  uint32_t sensorModeIndex);
 
 /**
@@ -61,7 +58,7 @@ Argus::SensorMode* getSensorMode(Argus::CameraDevice* cameraDevice,
  *
  * @param[in] cameraDevice The CameraDevice to get the WDR sensor mode from.
  */
-Argus::SensorMode* getWdrSensorMode(Argus::CameraDevice* cameraDevice);
+Argus::SensorMode *getWdrSensorMode(Argus::CameraDevice *cameraDevice);
 
 /**
  * Prints out information about a CameraDevice.
@@ -69,7 +66,8 @@ Argus::SensorMode* getWdrSensorMode(Argus::CameraDevice* cameraDevice);
  * @param[in] cameraDevice The device to print the info for.
  * @param[in] indent the indent to prepend to each line of output.
  */
-void printCameraDeviceInfo(Argus::CameraDevice* cameraDevice, const char* indent);
+void printCameraDeviceInfo(Argus::CameraDevice *cameraDevice,
+                           const char *indent);
 
 /**
  * Prints out information about a SensorMode.
@@ -77,12 +75,12 @@ void printCameraDeviceInfo(Argus::CameraDevice* cameraDevice, const char* indent
  * @param[in] sensorMode The mode to print the info for.
  * @param[in] indent the indent to prepend to each line of output.
  */
-void printSensorModeInfo(Argus::SensorMode* sensorMode, const char* indent);
+void printSensorModeInfo(Argus::SensorMode *sensorMode, const char *indent);
 
 /**
  * Prints out a UUID.
  */
-void printUUID(const Argus::UUID& uuid);
+void printUUID(const Argus::UUID &uuid);
 
 }; // namespace ArgusHelpers
 
